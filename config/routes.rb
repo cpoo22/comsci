a1321aa549912cb9fe67f320a3ef70acd764bcb5
Chris Chamberlain <cpoo22@yahoo.co.uk>
2012-06-10 14:11:40 +0100

    Minor tidy up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Comsci::Application.routes.draw do
-  resources :products
+  resources :products, :customers
 
-  resources :customers
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
