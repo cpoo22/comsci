3b75c2209443b52c9ad62c7e223fcaad5e214d1a
Chris Chamberlain <cpoo22@yahoo.co.uk>
2012-08-15 07:25:39 +0100

    Renamed factory correctly

--- spec/factories/order_item_factory.rb
+++ spec/factories/order_item_factory.rb
@@ -1,10 +1,12 @@
 FactoryGirl.define do
   factory :order_item do
-    price 1.5
+    unit_price 1.5
     product_code "5000"
-    quantity 1
+    quantity 2
     discount 1.5
     weight 1
     product_name "Becks Silver Zapper"
+    items_price 3
+    order
   end
 end
