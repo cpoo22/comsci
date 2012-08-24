f9a262ac2f08c4b5f0f360d966cc705d8966aa35
Chris Chamberlain <cpoo22@yahoo.co.uk>
2012-08-24 07:32:10 +0100

    Added customer search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,14 @@ class PagesController < ApplicationController
 
   def search
     if request.post?
-      @results = ['John Doe']
+      @customer_results = {}
+      customers = Customer.find_all_by_first_name(params[:search_param.downcase])
+      customers.concat   Customer.find_all_by_last_name(params[:search_param.downcase])
+      customers.concat   Customer.find_all_by_postcode(params[:search_param.downcase])
+      customers.each do |customer|
+        @customer_results[customer_path(customer)] =
+            "#{customer.first_name} #{customer.last_name}, #{customer.address_line_1}, #{customer.postcode}"
+      end
     end
   end
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,11 +15,48 @@ describe PagesController do
     response.should render_template(:search)
   end
 
-  it "post search should " do
-    FactoryGirl.create(:customer)
-    post :search, :search_param => 'doe'
-    assigns[:results].should_not be_nil
-    assigns[:results].should == ["John Doe"]
+  describe "searching" do
+    before(:each) do
+      FactoryGirl.create(:customer)
+      FactoryGirl.create(:product)
+    end
+
+    context "for customer" do
+      it "should set results" do
+        post :search, :search_param => 'doe'
+        assigns[:customer_results].should_not be_nil
+      end
+
+      it "should find customer on first name (case insensitive)" do
+        post :search, :search_param => 'jOhN'
+        assigns[:customer_results].values.should include 'John Doe, 22 Sharon Lane, TH78 6YH'
+      end
+
+      it "should find customer on last name (case insensitive)" do
+        post :search, :search_param => 'dOe'
+        assigns[:customer_results].values.should include 'John Doe, 22 Sharon Lane, TH78 6YH'
+      end
+
+      it "should find customer on postcode (case insensitive)" do
+        post :search, :search_param => 'Th78 6Yh'
+        assigns[:customer_results].values.should include 'John Doe, 22 Sharon Lane, TH78 6YH'
+      end
+
+      it "should find all customers matching searh param" do
+        FactoryGirl.create(:customer, first_name: 'Jane')
+        post :search, :search_param => 'Doe'
+        assigns[:customer_results].values.should include 'John Doe, 22 Sharon Lane, TH78 6YH'
+        assigns[:customer_results].values.should include 'Jane Doe, 22 Sharon Lane, TH78 6YH'
+        assigns[:customer_results].values.size.should == 2
+      end
+
+      it "should be empty if there are no results" do
+        post :search, :search_param => 'Ploppy poo pants'
+        assigns[:customer_results].should_not be_nil
+        assigns[:customer_results].should be_empty
+      end
+    end
+
   end
 
 end
